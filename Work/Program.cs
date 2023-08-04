36e0d9024a6c00b5654dbb88f93f6808812ea11a
Elvina <Elka121194@yandex.ru>
2023-08-04 17:46:56 +0300

    Added a flowchart

---
+++ Work/Program.cs
@@ -0,0 +1,28 @@
+﻿// Метод печати массива.
+void Print1DArr(string[] arr)
+{
+  Console.Write("[");
+  for (int i = 0; i < arr.Length - 1; i++)
+  {
+    Console.Write(arr[i] + ", ");
+  }
+  Console.WriteLine(arr[arr.Length - 1] + "]");
+}
+
+string[] array = { "Hello", "2", "world", ":-)", "1234", "- 2", "computer science", "Denmark", "Kazan" };
+string[] newArr = new string[0];
+int indexNewArr = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+  if (array[i].Length <= 3)
+  {
+    Array.Resize(ref newArr, newArr.Length + 1);
+    newArr[indexNewArr] = array[i];
+    indexNewArr++;
+  }
+}
+
+Print1DArr(array);
+Print1DArr(newArr);
+
